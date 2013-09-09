59ccb7e9de132ca55da8f0539f49f8a0cfb64b2d
Magesh <tkmagesh77@gmail.com>
2013-09-09 15:41:59 +0530

    Serialization Demo

---
+++ Day-4/CollectionsDemo/CollectionsDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollectionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary
+            Dictionary<int,string> someList = new Dictionary<int, string>();
+            someList.Add(1,"value - 1");
+            someList.Add(2, "value - 2");
+            someList.Add(3, "value - 3");
+            someList.Add(4, "value - 4");
+            foreach (var item in someList)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value);
+            }
+            Console.WriteLine("Value of Item with key = 2 is {0}", someList[2]);
+            Console.WriteLine("Attempting to add a duplicate item");
+            try
+            {
+                someList.Add(2, "something else");
+
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Dont try to add a duplicate item");
+            }
+            Console.WriteLine("Is item with key 3 exists - {0}", someList.ContainsKey(3));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day-4/SerializationDemo/SerializationDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SerializationDemo
+{
+    [Serializable]
+    public class Employee
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        [XmlElement(ElementName = "DepartmentNumber")]
+        public int DeptNo { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}", Id, FirstName, LastName, Salary, DeptNo);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Binary Serialization
+            /*var employee = new Employee() {Id = 101, FirstName = "Magesh", LastName = "Kuppan", Salary = 10000, DeptNo = 10};
+            var formatter = new BinaryFormatter();
+            FileStream fileStream = File.Create("employee.dat");
+            formatter.Serialize(fileStream,employee);
+            fileStream.Close();*/
+
+            //Binary Deserialization
+            /*var formatter = new BinaryFormatter();
+            FileStream fileStream = File.OpenRead("employee.dat");
+            var employee = (Employee) formatter.Deserialize(fileStream);
+            Console.WriteLine(employee);*/
+
+            //Xml Serialization
+            var serializer = new XmlSerializer(typeof(Employee));
+            var employee = new Employee() { Id = 101, FirstName = "Magesh", LastName = "Kuppan", Salary = 10000, DeptNo = 10 };
+            var writer = new StreamWriter("employee.xml");
+            serializer.Serialize(writer,employee);
+            writer.Close();
+
+            //Xml Deserialization
+            /*var serializer = new XmlSerializer(typeof (Employee));
+            var reader = new StreamReader("employee.xml");
+            var employee = (Employee) serializer.Deserialize(reader);
+            Console.WriteLine(employee);*/
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
