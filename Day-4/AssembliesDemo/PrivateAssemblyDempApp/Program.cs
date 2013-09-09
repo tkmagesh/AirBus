8e2c443f0cd9f919c4b8455aec68172945001817
Magesh <tkmagesh77@gmail.com>
2013-09-09 16:39:05 +0530

    Day - 4 Complete

---
+++ Day-4/AssembliesDemo/PrivateAssemblyDempApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AirBusUtilities;
+
+namespace PrivateAssemblyDempApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            Console.WriteLine(calc.Add(10,20));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day-4/AssembliesDemo/PublicAssemblyDemoApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AirBusUtilities;
+
+namespace PublicAssemblyDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(10,20));
+            Console.ReadLine();
+        }
+    }
+}
